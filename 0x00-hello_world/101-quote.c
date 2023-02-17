344f61fc03fc807e58086467f5321be61282c458
tiise101 <tiisetsomokgadi@yahoo.com>
2023-02-17 13:15:44 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * main - a program that prints a line to the standard error
- * return: 1
+ * Return: this returns 1
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
