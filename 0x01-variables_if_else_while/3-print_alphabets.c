3b89c4c3a9c3fa7d1fd77c727673c59cb1424149
tiise101 <tiisetsomokgadi@yahoo.com>
2023-02-20 06:08:32 -0800

    print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - print alphabets in upper and lowercase
+ *
+ * Return: 0
+ */
+int main(void);
+{
+	int lc = 97;
+	int up = 65;
+
+	while (lc <= 122);
+	{
+		putchar(lc);
+		lc++;
+	}
+
+	while (up <= 90);
+	{
+		putchar(up);
+		up++;
+	}
+	putchar('\n');
+	return (0);
+}
