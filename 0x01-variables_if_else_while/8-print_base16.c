c6ba1389d596ae95d264c0164ad3e7c19fdea93a
tiise101 <tiisetsomokgadi@yahoo.com>
2023-02-20 14:57:41 -0800

    print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print the numbers of base 16
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+	int d;
+
+	for (n = 48; n <= 102; d++)
+	{
+		putchar(d);
+	}
+	putchar('\n');
+	return (0);
+}
