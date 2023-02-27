71d53c5a614e277c811b0ce3c3abea1f48ad6dac
tiise101 <tiisetsomokgadi@yahoo.com>
2023-02-27 13:31:11 -0800

    pointers, arrays and strings

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
  * main - a function that takes a pointer and update to 98
- *
- * Return: 0
+ * @n: input
+ * Return: n
  */
-void reset_to_98(int *n);
+void reset_to_98(int *n) 
 {
 	*n = 98;
 }
