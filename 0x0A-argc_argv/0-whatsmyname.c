c944d963e6577783808c7356252c532afd9d6104
tiise101 <tiisetsomokgadi@yahoo.com>
2023-03-14 07:26:50 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  * @argv: array of arguments
  * Return: 0 (success)
  */
-int main(int argc __attribute__((unused)), char * argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
 
--- 0x0A-argc_argv/_putchar.c
+++ 0x0A-argc_argv/_putchar.c
@@ -5,7 +5,7 @@
  * _putchar - writes the character c to stdout
  * @c: The r character to print
  * Return: 1
- * On error, -1 and error is set
+ * On error, -1 and error is set appropriately
  */
 int _putchar(char c)
 {
